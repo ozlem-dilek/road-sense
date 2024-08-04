2030ec05cefb39c628980b17680b15679e769ea8
Ozlem Dilek Acar <acarozlemdilek@gmail.com>
2024-08-04 17:48:35 +0300

    welcome page updated.

--- lib/views/welcome.dart
+++ lib/views/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:road_damage_detection_app/views/login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key, title = "Welcome"});
@@ -13,55 +14,82 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 50),
-            Center(
-              child: SizedBox(
-                  height: 300,
-                  width: 300,
-                  child: Image(
-                      image: AssetImage("lib/assets/images/3.jpg"))),
+      body: Stack(
+        children: [
+          // Background image
+          Positioned.fill(
+            child: Image.asset(
+              "lib/assets/images/7.png",
+              fit: BoxFit.cover,
             ),
-            SizedBox(height: 10),
-            Text("RoadSense",
-              style: TextStyle(
-                  fontSize: 45,
-                  fontWeight: FontWeight.w700)),
-            Padding(
-              padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
-              child: Text("Detect road damages, recieve real-time notifications and safe driving!",
-              style: TextStyle(
-                fontSize: 20,
-              ),),
+          ),
+          // Semi-transparent overlay
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.5), // Optional: to make text more readable
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-              child: ElevatedButton(
-                  onPressed:(){
-                    Navigator.push(
+          ),
+          // Content
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                Text(
+                  "ROADSENSE",
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 60,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 8,
+                    color: Colors.white, // Text color for better contrast
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.35),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    "Yol Hasarlarını Anında Tespit Edin, Güvenli Sürüşe Devam Edin!",
+                    style: GoogleFonts.josefinSans(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white, // Text color for better contrast
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => LoginPage()));
-                  },
-                  child: Text('Get Started!',
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    child: Text(
+                      'Başlayalım!',
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                      fontWeight: FontWeight.w700)),
-                  style: ButtonStyle(
-                        minimumSize: WidgetStatePropertyAll(Size(300,70)),
-                        backgroundColor: WidgetStatePropertyAll(Colors.orangeAccent)
-                  )
-              ),
-            )
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 70),
+                      backgroundColor: Colors.orangeAccent,
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
 
-          ],
-        ),
-      )
-
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
